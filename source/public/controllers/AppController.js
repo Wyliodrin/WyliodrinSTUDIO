9355b9922eca68152d07d8ae369ec96abbed6e8e
Paula-Elena <margarit.paula@gmail.com>
2016-04-02 13:15:23 +0300

    Added shortcut keys for submit( 'Enter' ) and exit( 'Escape' ). Modified the following files:
    Enter:
    - add-app.html
    - authenticate.html
    - rename.html
    - signal-properties.html
    
    Escape:
    - about.html
    - add-app.html
    - authenticate.html
    - board.html
    - firmware-example.html
    - license.html
    - network.html
    - package-manager.html
    - rename.html
    - signal-properties.html
    - software-example.html
    - task-manager.html
    - update.html
    - welcome.html
    - wyliodrin-userinterface.html

--- source/public/controllers/AppController.js
+++ source/public/controllers/AppController.js
@@ -107,6 +107,34 @@ module.exports = function ()
 		setBoxSize ();
 	});
 	
+	app.directive('ngEnter', function () {
+		return function (scope, element, attrs) {
+			element.bind("keydown keypress", function(event) {
+				if (event.which === 13) {
+					scope.$apply(function () {
+						scope.$eval(attrs.ngEnter);
+					});
+
+					event.preventDefault();
+				}
+			});
+		};
+	});
+
+	app.directive('ngEscape', function () {
+		return function (scope, element, attrs) {
+			element.bind("keydown keypress", function(event) {
+				if (event.which === 27) {
+					scope.$apply(function () {
+						scope.$eval(attrs.ngEscape);
+					});
+
+					event.preventDefault();
+				}
+			});
+		};
+	});
+
 	function setBoxSize ()
 		{
 			$('.tabs-box-container').height( $(window).height() - $('.main-toolbar').height() );

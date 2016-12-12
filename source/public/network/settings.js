9607fce0458b5ebcb265ca835087892652b32df7
ioanaculic <ioana_mculic@yahoo.com>
2016-12-12 12:49:00 -0500

    settings

---
+++ source/public/network/settings.js
@@ -0,0 +1,74 @@
+module.exports.boards = {
+	'board':
+	{
+		'software':'Software',
+		'firmware':'Firmware',
+		text: 'Embedded Device',
+		picture: '/public/drawable/board.png'
+	},
+	'raspberrypi':
+	{
+		'software':'Raspberry Pi',
+		'firmware':'Arduino',
+		text: 'Raspberry Pi',
+		username: 'pi',
+		password: 'raspberry',
+		picture: '/public/drawable/raspberrypi.png'
+	},
+	'udooneo':
+	{
+		'software':'UDOO Neo',
+		'firmware':'Arduino',
+		text: 'UDOO Neo',
+		username: 'udooer',
+		password: 'udooer',
+		picture: '/public/drawable/udooneo.png'
+	},
+	'arduinoyun':
+	{
+		'software':'Arduino Yun',
+		'firmware':'Arduino',
+		text: 'Arduino Yun',
+		username: 'root',
+		password: 'doghunter',
+		picture: '/public/drawable/arduinoyun.png'
+	},
+	'beagleboneblack':
+	{
+		'software':'BeagleBone Black',
+		'firmware':'',
+		text: 'BeagleBone Black',
+		username: 'udooer',
+		password: 'udooer',
+		picture: '/public/drawable/beagleboneblack.png'
+	},
+	'chrome':
+	{
+		'software':'JavaScript',
+		'firmware':'',
+		text:'Chrome',
+		picture: '/public/drawable/chrome.png'
+	},
+	'dragonboard':
+	{
+		'software':'DragonBoard 410C',
+		'firmware':'',
+		text: 'DragonBoard 410C',
+		usename: '',
+		password: '',
+		picture: '/public/drawable/dragonboard.png'
+	},
+	'cisco829':
+	{
+		'software':'Cisco IR 829',
+		'firmware':'',
+		text: 'Cisco IR 829',
+		usename: 'root',
+		password: '',
+		picture: '/public/drawable/cisco829.png'
+	},
+	'openmot':
+	{
+		picture: '/public/drawable/openmote.png'
+	}
+};

124f5371f3d3200b0c8b723031617cddfaaf0592
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2017-01-18 15:12:58 +0200

    notebook angular

---
+++ source/public/notebook/notebook.js
@@ -0,0 +1,30 @@
+
+"use strict";
+
+var $ = require ('jquery');
+
+window.jQuery = $;
+
+var angular = require ('angular');
+var angular_material = require ('angular-material');
+
+var app = angular.module ("wyliodrinApp", ['ngMaterial'], function ($provide)
+{
+	$provide.decorator('$window', function($delegate) 
+	{
+      try
+      {
+        $delegate.history = null;
+      }
+      catch (e)
+      {
+        
+      }
+      return $delegate;
+    });
+});
+
+
+
+
+
